de54d11cfe0186b703892013141e5f02ec7317bf
brtyvz <beratttyavuzz@gmail.com>
2021-12-14 13:43:36 +0300

    Initial Commit

---
+++ tableView_ikili/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView_ikili
+//
+//  Created by Berat Yavuz on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
